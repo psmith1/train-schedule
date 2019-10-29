b2b7de8523afd94e76eb9285a73f1f98167c4690
Victor Uland <53414263+vuduland@users.noreply.github.com>
2019-10-29 10:17:32 -0500

    comparing solutions and conflicts

--- assets/javascript/train-times.js
+++ assets/javascript/train-times.js
@@ -1,108 +1,101 @@
-var config = {
-    apiKey: "AIzaSyBpjLLBBvAHzUyTx6gy33zKH8XIDhr2A-o",
-    authDomain: "timesheetproject-14ffb.firebaseapp.com",
-    databaseURL: "https://timesheetproject-14ffb.firebaseio.com",
-    projectId: "timesheetproject-14ffb",
-    storageBucket: "timesheetproject-14ffb.appspot.com",
-    messagingSenderId: "1000606389209",
-    appId: "1:1000606389209:web:608489380ee262e79b3fc8",
-    measurementId: "G-3HRMENMREJ"
-  };
+// Create a variable to reference the database.
+let database = firebase.database();
 
-  firebase.initializeApp(config);
+// Initial Values
+let trainName = "";
+let destination = "";
+let firstDeparture = null;
+let arrivalTime = 0;
+let frequency = 0;
+let minutesAway = 0;
+let newRow = "";
 
-  // Create a variable to reference the database.
-  var database = firebase.database();
-
-  // Initial Values
-  var trainName = "";
-  var destination = "";
-  var firstTrain = "";
-  var frequency = 0;
-  var minutesAway = null;
-  var nextTrain = null;
-
-
-  // Capture Button Click
-  $("#submitbtn").on("click", function(event) {
+// Capture Button Click
+$("#submitbtn").on("click", function (event) {
     event.preventDefault();
-    alert("Form submitted");
 
-    // Grabbed values from text boxes
     trainName = $("#inputTrainName").val().trim();
     destination = $("#inputDestination").val().trim();
-    firstTrain = $("#inputFirstTrain").val().trim();
+    firstDeparture = $("#inputFirstDeparture").val().trim();
     frequency = $("#inputFrequency").val().trim();
 
-    // Code for handling the push
-    database.ref().push({
-      trainName: trainName,
-      destination: destination,
-      firstTrain: firstTrain,
-      frequency: frequency,
-     });
 
-// Create the new row
-var newRow = $('<tr>').append(
-    $('<td>').text(trainName),
-    $('<td>').text(destination),
-    $('<td>').text(frequency),
-    $('<td>').text(nextTrain),
-    $('<td>').text(minutesAway)
+    // Creates local "temporary" object for holding employee data
+    let newTrain = {
+        name: trainName,
+        destination: destination,
+        frequency: frequency,
+        firstDeparture: firstDeparture,
+        arrivalTime: arrivalTime,
+        minutesAway: minutesAway
+    };
 
-  );
-  // Append the new row to the table
-  calculateTime();
-  $("#trainList").append(newRow);
-  });
 
-  function calculateTime() {
+    // Uploads train data to the database
+    doMath();
+    database.ref().push(newTrain);
+    database.ref().on("child_added", function () {
+
+        $("#trainList").append(newRow);
+    });
+
+    let newRow = $("<tr>").append(
+        $("<td>").text(trainName),
+        $("<td>").text(destination),
+        $("<td>").text(frequency),
+        $("<td>").text(firstDeparture),
+        $("<td>").text(arrivalTime),
+        $("<td>").text(minutesAway)
+
+    );
+
+
+
+    // database.ref().push({
+    //     trainName: trainName,
+    //     destination: destination,
+    //     frequency: frequency,
+    //     firstDeparture: firstDeparture,
+    //     arrivalTime: arrivalTime,
+    //     minutesAway: minutesAway
+    // });
+
+
+
+    // console.log(snapshot.val());
+    // console.log(snapshot.val().trainName);
+    // console.log(snapshot.val().destination);
+    // console.log(snapshot.val().firstDeparture);
+    // console.log(snapshot.val().arrivalTime);
+    // console.log(snapshot.val().frequency);
+    // console.log(snapshot.val().minutesAway);
+});
+
+
+let doMath = function () {
+
     console.log(frequency);
 
-      // Assumptions
-      // var tFrequency = $('<td>').text(frequency);
-      // console.log(frequency);
+    let firstDepartureConverted = moment(firstDeparture, "HH:mm").subtract(1, "years");
+    console.log(firstDepartureConverted);
 
-      // Time is 3:30 AM
-      // var firstTime = database.ref(firstTrain);
-      // console.log(firstTime);
-      // First Time (pushed back 1 year to make sure it comes before current time)
-      var firstTimeConverted = moment(firstTrain, "HH:mm").subtract(1, "years");
-      console.log(firstTimeConverted);
-  
-      // Current Time
-      var currentTime = moment();
-      console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-  
-      // Difference between the times
-      var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-      console.log("DIFFERENCE IN TIME: " + diffTime);
-  
-      // Time apart (remainder)
-      var remainder = diffTime % frequency;
-      console.log(remainder);
-  
-      // Minute Until Train
-      var minutesAway = frequency - remainder;
-      console.log("MINUTES TILL TRAIN: " + minutesAway);
+    let currentTime = moment();
+    console.log("Current Time: " + moment(currentTime).format("hh:mm"));
 
-      //next train
-      var nextTrain = moment().add(minutesAway, "minutes");
-      console.log("Arrival time: " + moment(nextTrain).format("hh:mm A"));
-  }
+    //difference between the times
+    let diffTime = moment().diff(moment(firstDepartureConverted), "minutes");
+    console.log("Difference in Times: " + diffTime);
 
+    //Time apart (remainder)
+    let remainder = diffTime % frequency;
+    console.log("remainder: " + remainder);
 
-// // Firebase watcher + initial loader HINT: .on("value")
-// database.ref().on("value", function(snapshot) {
+    //minute until train arrival
+    minutesAway = frequency - remainder;
+    console.log("Minutes until train arrival: " + minutesAway);
 
-//   // Log everything that's coming out of snapshot
-//   console.log(snapshot.val());
-//   console.log(snapshot.val().name);
-//   console.log(snapshot.val().email);
-//   console.log(snapshot.val().age);
-//   console.log(snapshot.val().comment);
+    //next train
+    arrivalTime = moment().add(minutesAway, "minutes");
+    console.log("Arrival time: " + moment(arrivalTime).format("hh:mm"));
 
-  // Change the HTML to reflect
-  // $("#name-display").text(snapshot.val().nextTrain);
-
-// }
+};
